3527dab8e79da7d291fb55fbc27948fd1f6dbd30
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-04-27 21:12:10 +0900

    [domain] add Block struct

---
+++ domain/block.go
@@ -0,0 +1,34 @@
+package domain
+
+import (
+	"io"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/rlp"
+)
+
+type Block struct {
+	Txes      []*Tx
+	Signature Signature
+}
+
+func NewBlock(txes []*Tx) *Block {
+	return &Block{
+		Txes: txes,
+	}
+}
+
+// implements RLP Encoder interface
+// ref. https://godoc.org/github.com/ethereum/go-ethereum/rlp#Encoder
+func (block *Block) EncodeRLP(w io.Writer) error {
+	return rlp.Encode(w, []interface{}{block.Txes, block.Signature})
+}
+
+func (block *Block) Hash() (Hash, error) {
+	b, err := rlp.EncodeToBytes([]interface{}{block.Txes})
+	if err != nil {
+		return Hash{}, nil
+	}
+
+	return NewHashFromBytes(crypto.Keccak256(b)), nil
+}
